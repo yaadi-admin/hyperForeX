fccdbd16e89c23119acc7499a0bd325d7ec6743b
luo <luoxianmingg@gmail.com>
2023-06-22 23:38:20 -0400

    body parse

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const { Wallets, Gateway } = require('fabric-network');
 const FabricCAServices = require('fabric-ca-client');
 const testNetworkRoot = path.resolve(require('os').homedir(), 'fabric-samples/test-network');
 app.use(express.json());
-
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/enrollUser', async (req, res) => {
     try {
